f2ff180438a286228e4dd000185fda2b5bec2ade
alan.anaya <alan.anaya@softtek.com>
2020-03-09 16:05:43 -0600

    Script addition

--- script.sh
+++ script.sh
@@ -8,8 +8,8 @@ let substraction=$var1-$var2
 let multiplication=$var1*$var2
 let division=$var1/$var2
 
-echo $addition
-echo $substraction
-echo $multiplication
-echo $division
+echo 'The result of the addition is: $addition'
+echo 'The result of the substraction is: $substraction'
+echo 'The result of the multiplication is: $multiplication'
+echo 'The result of the division is: $division'
 
