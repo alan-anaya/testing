b3255ad3b7c050605d767e346fb657a57267db48
alan.anaya <alan.anaya@softtek.com>
2020-03-09 09:28:27 -0600

    add file

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bashi
+
+var1=$1
+var2=$2
+
+let addition=$var1+$var2
+let substraction=$var1-$var2
+let multiplication=$var1*$var2
+let division=$var1/$var2
+
+echo $addition
+echo $substraction
+echo $multiplication
+echo $division
+
